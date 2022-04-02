25b93864752777db0c5e5ee6cab4e22addc6b697
narafu <narafu@kakao.com>
2022-04-02 11:03:22 +0900

    section_02

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -26,11 +26,17 @@ public class Category {
     private List<Item> items = new ArrayList<>();
     // 다대다는 변형이 어려우므로, 이대로 사용하기보다는 일대다와 다대일로 찢어서 설계한다.
 
-    @ManyToOne(fetch = FetchType.LAZY)(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
+
+    // 연관관계 메서드
+    public void addChildCategory(Category child) {
+        this.child.add(child);
+        child.setParent(this);
+    }
 
 }
