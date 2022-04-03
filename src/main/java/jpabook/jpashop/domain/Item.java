fb5df2b0ffc4bd483f4ab2634e08fc1f107d1c4d
narafu <narafu@kakao.com>
2022-04-03 15:45:56 +0900

    section_04

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,26 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    // 비즈니스 로직
+    // 데이터가 있는 클래스에 비즈니스 로직이 있는 것이 응집도가 높다.
+    // getter를 가져와서 지지고 볶고 다시 set 해주지 말자.
+    // setter 없이 addStock()과 removeStock()을 사용하자.
+
+    /*
+     * stock 증가
+     * */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /*
+     * stock 감소
+     * */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
