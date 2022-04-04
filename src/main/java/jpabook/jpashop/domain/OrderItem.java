da2ab635a4ff9dc08a2b7dca436e43ab4c880cd8
narafu <narafu@kakao.com>
2022-04-04 22:56:20 +0900

    section_06

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -8,6 +10,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // protected 기본생성자와 같은 코드 - 기본생성자 방지
 public class OrderItem {
 
     @Id
@@ -20,11 +23,40 @@ public class OrderItem {
     private Item item;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="order_id")
+    @JoinColumn(name = "order_id")
     private Order order;
 
     private int orderPrice; // 주문 가격
-
     private int count; // 주문 수량
+
+    // @NoArgsConstructor(access = AccessLevel.PROTECTED) 때문에 주석
+//    protected OrderItem() {
+//    }
+
+    // 생성 메서드
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    // 비즈니스 로직
+    /*
+     * 재고수량 원복
+     * */
+    public void cancel() {
+        this.getItem().addStock(count);
+    }
+
+    // 조회 로직
+    /*
+     * 주문 상품 전체 가격 조회
+     * */
+    public int getTotalPrice() {
+        return this.getOrderPrice() * this.getCount();
+    }
 
 }
